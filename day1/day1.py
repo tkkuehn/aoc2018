9bfec6585bb70f9fac6e19cd94c4056f86e1baa0
Tristan Kuehn <tristankuehn@gmail.com>
2018-12-03 11:46:46 -0500

    Remove old file

--- day1/day1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-with open('./resources/input.txt', 'r') as f:
-    contents = f.read().splitlines()
-    int_contents = [int(x) for x in contents]
-    print(sum(int_contents))
-
