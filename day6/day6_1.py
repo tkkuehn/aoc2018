8edcefacecba865b7e4037deeb8c1e4b7f32048b
Tristan Kuehn <tristankuehn@gmail.com>
2018-12-06 13:15:18 -0500

    Solve day 6 part 1

--- day6/day6_1.py
+++ day6/day6_1.py
@@ -5,63 +5,57 @@ with open('./resources/input.txt', 'r') as f:
 
     given_points = set([(int(x.split(',')[0]), int(x.split(',')[1]))
         for x in contents])
+    claimed_points = set()
+    contested_points = set()
+    claimed_index = {}
+
+    for point in given_points:
+        claimed_index[point] = set()
 
     distance = 1
 
-    claimed = set()
-    equidistant = set()
-    closest_points = {}
-    points_claimed = {}
     infinite = set()
-
-    for point in given_points:
-        points_claimed[point] = set()
 
     unfinished_points = given_points
 
-    inf_size = 500
+    # Just some large number
+    inf_size = 250
+
     while distance < inf_size:
-        claimed_this_time = set()
-        points_to_remove = set()
-        for point in unfinished_points:
-            claimed_new_point = False
+        known_points = given_points | claimed_points | contested_points
+        claims_index = {}
+        for point in given_points:
             for x_distance in range(distance + 1):
-                for multiplier in [-1, 1]:
-                    new_point = (point[0] + (multiplier * x_distance),
-                            point[1] + (multiplier * (distance - x_distance)))
-                    if new_point in claimed:
+                for x_multiplier in [-1, 1]:
+                  for y_multiplier in [-1, 1]:
+                    new_point = (point[0] + (x_multiplier * x_distance),
+                            point[1] + (y_multiplier * (distance - x_distance)))
+                    if new_point in known_points:
                         pass # Don't do anything
-                    elif new_point in claimed_this_time:
-                        equidistant.add(new_point)
-                        points_claimed[closest_points[new_point][0]].remove(
-                                new_point)
-                        closest_points[new_point].append(point)
                     else:
-                        closest_points[new_point] = [point]
-                        points_claimed[point].add(new_point)
-                        claimed_this_time.add(new_point)
-                        claimed_new_point = True
-            if not claimed_new_point:
-                points_to_remove.add(point)
-        claimed = claimed | claimed_this_time
-        unfinished_points -= points_to_remove
+                        if new_point in claims_index:
+                            claims_index[new_point].add(point)
+                        else:
+                            claims_index[new_point] = set([point])
+        for new_point in claims_index:
+            if len(claims_index[new_point]) > 1:
+                contested_points.add(new_point)
+                    
+            else:
+                claimed_points.add(new_point)
+                claimer = claims_index[new_point].pop()
+                claimed_index[claimer].add(new_point)
+
+                # If still claiming points, this point is probably infinite
+                if distance == inf_size - 1:
+                    infinite.add(claimer) 
         distance += 1
-
-    for point in given_points:
-        for x_distance in range(distance + 1):
-            for multiplier in [-1, 1]:
-                new_point = (point[0] + (multiplier * x_distance),
-                        point[1] + (multiplier * (distance - x_distance)))
-                if new_point in claimed:
-                    pass # Don't do anything
-                else:
-                    infinite.add(point)
-
+            
     max_area = 0
     largest_area = (0, 0)
     for point in (given_points - infinite):
-        if len(points_claimed[point]) > max_area:
-            max_area = len(points_claimed[point])
+        if len(claimed_index[point]) > max_area:
+            max_area = len(claimed_index[point]) + 1
             largest_area = point
 
     print(max_area)
