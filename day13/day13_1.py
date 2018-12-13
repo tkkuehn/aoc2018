bdb6dacfcc2a8a4f8ddde24ccbc974716c6b88f5
Tristan Kuehn <tristankuehn@gmail.com>
2018-12-13 10:18:42 -0500

    Solve day 13 part 1

--- day13/day13_1.py
+++ day13/day13_1.py
@@ -2,44 +2,69 @@
 
 
 contents = []
-with open('./resources/test_input.txt', 'r') as f:
+with open('./resources/input.txt', 'r') as f:
     contents = f.read().splitlines()
 
 track = {}
 carts = []
+
+y_len = len(contents)
+x_len = max([len(x) for x in contents])
 
 for i in range(len(contents)):
     line = contents[i]
     for j in range(len(line)):
         section = line[j]
         if section == '|':
-            track[(i, j)] = 'vertical'
+            track[(j, i)] = 'vertical'
         elif section == '-':
-            track[(i, j)] = 'horizontal'
+            track[(j, i)] = 'horizontal'
         elif section == '^':
-            track[(i, j)] = 'vertical'
-            carts.append({'pos': (i, j), 'facing': 'up'})
+            track[(j, i)] = 'vertical'
+            carts.append({'pos': (j, i), 'facing': 'up'})
         elif section == 'v':
-            track[(i, j)] = 'vertical'
-            carts.append({'pos': (i, j), 'facing': 'down'})
+            track[(j, i)] = 'vertical'
+            carts.append({'pos': (j, i), 'facing': 'down'})
         elif section == '>':
-            track[(i, j)] = 'horizontal'
-            carts.append({'pos': (i, j), 'facing': 'right'})
+            track[(j, i)] = 'horizontal'
+            carts.append({'pos': (j, i), 'facing': 'right'})
         elif section == '<':
-            track[(i, j)] = 'horizontal'
-            carts.append({'pos': (i, j), 'facing': 'left'})
+            track[(j, i)] = 'horizontal'
+            carts.append({'pos': (j, i), 'facing': 'left'})
         elif section == '\\':
-            if (j + 1 < len(line)) and line[j + 1] == '-':
-                track[(i, j)] = 'up/right'
+            if (j + 1 < len(line)) and line[j + 1] in ('-', '<', '>', '+'):
+                track[(j, i)] = 'up/right'
             else:
-                track[(i, j)] = 'down/left'
+                track[(j, i)] = 'down/left'
         elif section == '/':
-            if (j + 1 < len(line)) and line[j + 1] == '-':
-                track[(i, j)] = 'down/right'
+            if (j + 1 < len(line)) and line[j + 1] in ('-', '<', '>', '+'):
+                track[(j, i)] = 'down/right'
             else:
-                track[(i, j)] = 'up/left'
+                track[(j, i)] = 'up/left'
         elif section == '+':
-            track[(i, j)] = 'junction'
+            track[(j, i)] = 'junction'
+        elif section == ' ':
+            pass
+        else:
+            print(f'Unknown character: {section}')
+
+for j in range(y_len):
+    line = []
+    for i in range(x_len):
+        if (i, j) in track:
+            if track[(i, j)] == 'vertical':
+                line.append('|')
+            elif track[(i, j)] == 'horizontal':
+                line.append('-')
+            elif track[(i, j)] == 'junction':
+                line.append('+')
+            elif track[(i, j)] in ['up/right', 'down/left']:
+                line.append('\\')
+            elif track[(i, j)] in ['down/right', 'up/left']:
+                line.append('/')
+        else:
+            line.append(' ')
+#    print(''.join(line))
 
 for cart in carts:
     cart['next_turn'] = 'left'
@@ -48,99 +73,107 @@ collision = False
 collision_site = (0, 0)
 t = 0
 while not collision:
+    carts.sort(key=lambda cart: cart['pos'][1])
+
     for cart in carts:
         pos = cart['pos']
         facing = cart['facing']
+
         next_pos = pos
         track_type = track[pos]
+
         if track_type == 'vertical':
             if facing == 'up':
-                next_pos[1] -= 1
+                next_pos = (pos[0], pos[1] - 1)
             else:
-                next_pos[1] += 1
+                next_pos = (pos[0], pos[1] + 1)
         elif track_type == 'horizontal':
             if facing == 'right':
-                next_pos[0] += 1
+                next_pos = (pos[0] + 1, pos[1])
             else:
-                next_pos[0] -= 1
+                next_pos = (pos[0] - 1, pos[1])
         elif track_type == 'up/right':
             if facing == 'down':
-                next_pos[0] += 1
+                next_pos = (pos[0] + 1, pos[1])
                 facing = 'right'
             else:
-                next_pos[1] -= 1
+                next_pos = (pos[0], pos[1] - 1)
                 facing = 'up'
         elif track_type == 'up/left':
             if facing == 'down':
-                next_pos[0] -= 1
+                next_pos = (pos[0] - 1, pos[1])
                 facing = 'left'
             else:
-                next_pos[1] -= 1
+                next_pos = (pos[0], pos[1] - 1)
                 facing = 'up'
         elif track_type == 'down/right':
             if facing == 'up':
-                next_pos[0] += 1
+                next_pos = (pos[0] + 1, pos[1])
                 facing = 'right'
             else:
-                next_pos[1] += 1
+                next_pos = (pos[0], pos[1] + 1)
                 facing = 'down'
         elif track_type == 'down/left':
             if facing == 'up':
-                next_pos[0] -= 1
+                next_pos = (pos[0] - 1, pos[1])
                 facing = 'left'
             else:
-                next_pos[1] += 1
+                next_pos = (pos[0], pos[1] + 1)
                 facing = 'down'
         elif track_type == 'junction':
             if cart['next_turn'] == 'left':
                 if facing == 'up':
-                    next_pos[0] -= 1
+                    next_pos = (pos[0] - 1, pos[1])
                     facing = 'left'
                 elif facing == 'right':
-                    next_pos[1] -= 1
+                    next_pos = (pos[0], pos[1] - 1)
                     facing = 'up'
                 elif facing == 'down':
-                    next_pos[0] += 1
+                    next_pos = (pos[0] + 1, pos[1])
                     facing = 'right'
                 elif facing == 'left':
-                    next_pos[1] += 1
+                    next_pos = (pos[0], pos[1] + 1)
                     facing = 'down'
                 cart['next_turn'] = 'straight'
             elif cart['next_turn'] == 'straight':
                 if facing == 'up':
-                    next_pos[1] -= 1
+                    next_pos = (pos[0], pos[1] - 1)
                 elif facing == 'right':
-                    next_pos[0] += 1
+                    next_pos = (pos[0] + 1, pos[1])
                 elif facing == 'down':
-                    next_pos[1] += 1
+                    next_pos = (pos[0], pos[1] + 1)
                 elif facing == 'left':
-                    next_pos[0] -= 1
+                    next_pos = (pos[0] - 1, pos[1])
                 cart['next_turn'] = 'right'
             elif cart['next_turn'] == 'right':
                 if facing == 'up':
-                    next_pos[0] += 1
+                    next_pos = (pos[0] + 1, pos[1])
                     facing = 'right'
                 elif facing == 'right':
-                    next_pos[1] += 1
+                    next_pos = (pos[0], pos[1] + 1)
                     facing = 'down'
                 elif facing == 'down':
-                    next_pos[0] -= 1
+                    next_pos = (pos[0] - 1, pos[1])
                     facing = 'left'
                 elif facing == 'left':
-                    next_pos[1] -= 1
+                    next_pos = (pos[0], pos[1] - 1)
                     facing = 'up'
                 cart['next_turn'] = 'left'
+        else:
+            print(f'Unknown track type: {track_type}')
         cart['pos'] = next_pos
         cart['facing'] = facing
 
-    cart_positions = set()
-    for cart in carts:
-        cart_pos = cart['pos']
-        if cart_pos in cart_positions:
-            collision = True
-            collision_site = cart_pos
-        else:
-            cart_positions.add(cart_pos)
+        cart_positions = set()
+        for new_cart in carts:
+            cart_pos = new_cart['pos']
+            if cart_pos in cart_positions:
+                collision = True
+                collision_site = cart_pos
+                break
+            else:
+                cart_positions.add(cart_pos)
+
     t += 1
 
 print(collision_site)
